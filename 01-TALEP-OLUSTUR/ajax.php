442660c6fd93f1a5948c5732c209408afe2e193a
Eser Öznohut <esero@outlook.com.tr>
2017-04-26 09:36:26 +0300

    09:30

--- 01-TALEP-OLUSTUR/ajax.php
+++ 01-TALEP-OLUSTUR/ajax.php
@@ -4,7 +4,15 @@ $pf = new ProcessFunctions();
 $option = $pf->fnc->post("OPTION");
 switch ($option)
 {
-    
+    case 'GET_PRODUCT':
+
+        $CODE = $pf->fnc->post("CODE");
+
+        $PRODUCT = $pf->bahadir->kanbanDb->Select("SELECT NAME FROM PRODUCTS WHERE CODE = '$CODE'")[0];
+
+        echo json_encode($PRODUCT);
+
+    break;
 
 }
 ?>
